153b463c7e080516da615ecd1d5919a678ef09cf
Tobymat <tobematthew@gmail.com>
2022-09-08 15:20:10 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
  * A printout program
+ * Main contains the entire program
+ * puts prints out the value
+ * return returns another number if incorrect
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle/n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
