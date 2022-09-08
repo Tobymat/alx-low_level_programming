dfeed9b40a3eab21e8a5338a35e807cb016c97b9
Tobymat <tobematthew@gmail.com>
2022-09-08 16:00:07 -0700

    add

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Program to printout a value
+ * Main function runs the code
+ * return no value if correct
+ */
+
+int main(void)
+{
+	printf(%s,"with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
