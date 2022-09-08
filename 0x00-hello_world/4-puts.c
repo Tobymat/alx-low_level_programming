58cbb84d18a10afe1cf6117619e615ad2747e2e0
Tobymat <tobematthew@gmail.com>
2022-09-08 15:38:24 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@ int main(void)
 /*main contains the program*/
 
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle\n");
 		return (0);
 	/*return returns a value if incorrect*/
 }
