1d98f58c0f4932fe16143098e49e4ae171f3c58e
Tobymat <tobematthew@gmail.com>
2022-09-10 06:12:45 -0700

    add

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - executes the code within the program
+ * write - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * Return: Always returns 1
+ */
+
+int main(void)
+{
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n",61);
+	return (1);
+}
