c83577cbafce17405d7fd8c5f4556bc487ae4b5b
Tobymat <tobematthew@gmail.com>
2022-09-12 14:32:47 -0700

    add

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * putchar - prints 'lowercase and uppercase alphabets'
+ * Return: 0 if false and 1 if true
+ */
+
+int main(void)
+{
+	char ah;
+
+	for (ah = 'a'; ah < 'z'; ah++)
+	{
+		putchar(ah);
+	} else if (ah = 'A'; ah < 'Z'; ah++)
+	{
+		putchar(ah);
+	}
+	putchar('\n');
+	return (0);
+}
