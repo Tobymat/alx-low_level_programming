e30f25dfde57745c4dab57269bd9cdc08383bec3
Tobymat <tobematthew@gmail.com>
2022-09-13 15:58:30 -0700

    add

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,12 @@
 
 void print_alphabet_x10(void)
 {
-	int i;
+	int i = 10;
 	char j;
 
-	for (i = 0; i < 10; i++)
+	for (j = 'a'; j <= 'z' j++)
 	{
-		for (j = 'a'; j <= 'z'; j++)
+		for (j < i; i <= 10; j++)
 		{
 			_putchar(j);
 		}
