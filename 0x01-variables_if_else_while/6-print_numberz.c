ab84696323454de79cb88c84f4ac0859e9bbae2c
Tobymat <tobematthew@gmail.com>
2022-09-12 17:17:59 -0700

    add

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (int i = 0; i < 10; i++)
 	{
-		putchar(i);
+		putchar(int i);
 	}
-	putchar('%d\n');
+	putchar('\n');
 	return (0);
 }
