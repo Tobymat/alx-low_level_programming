08f61defa3caa4e0d9fd578e51a04fa744ab279c
Tobymat <tobematthew@gmail.com>
2022-09-12 18:03:33 -0700

    add

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * putchar - prints combinations of single digits
+ * Return: 0 if false and 1 if otherwise
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
