d1d2f3c32eed56b66ab33a02cc23f9b9e2056e23
Tobymat <tobematthew@gmail.com>
2022-09-12 17:37:58 -0700

    add

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * putchar - prints lowercase alphabets in reverse
+ * Return: 0 if false and 1 if otherwise
+ */
+
+int main(void)
+{
+	while(sh = z && sh <= a)
+	{
+		putchar(sh);
+		sh++;
+	}
+	putchar('\n');
+	return (0);
+}
