1465dc99049292bdcd03fd79b265866049dc09e6
Tobymat <tobematthew@gmail.com>
2022-09-13 08:00:57 -0700

    add

---
+++ 0x02-functions_nested_loops/0.putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * putchar - prints input to standard output
+ * Return: 0 if false and 1 if otherwise
+ */
+
+int main(void)
+{
+	putchar('\n');
+	return (0);
+}
