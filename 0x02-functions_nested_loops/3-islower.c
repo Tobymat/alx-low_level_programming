bdbe6c34dc3d94ee5ebaf64d7f8d8255aa02dd77
Tobymat <tobematthew@gmail.com>
2022-09-13 17:23:13 -0700

    add

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - entry point
+ * Return: 0 if false and 1 if otherwise
+ */
+
+int _islower(int c)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		return (1\n);
+	}
+	return (0);
