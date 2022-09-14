24426c6a1a49d5ccc78db0827c64823a7cd3edb7
Tobymat <tobematthew@gmail.com>
2022-09-13 17:46:38 -0700

    add

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isaplha - checks if character is an alphabet
+ * @c: character to check
+ */
+
+int _isalpha(int c)
+{
+	return (c = 65 && c = 122);
+}
