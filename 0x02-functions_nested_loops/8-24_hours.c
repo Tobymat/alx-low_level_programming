2206471985210a9d8b2738b63963d62bbbf5cbd3
Tobymat <tobematthew@gmail.com>
2022-09-13 18:49:45 -0700

    add

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ * Return: 0 if false and 1 if otherwise
+ */
+
+void jack_bauer(void)
+{
+	int i;
+
+	for (i = 00:00; i <= 23:59; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
