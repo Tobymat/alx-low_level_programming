a36725bc1ac93b3343e824477ab9561489d5cdbe
Toby Matt <tobematthew@gmail.com>
2022-09-15 12:52:59 +0000

    add

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,31 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
  * main - entry point
- *
- * int - variable declaration and intialization
- *
- * Return: 0 if negative and 1 if otherwise
+ * Return: 0
  */
 
 int main(void)
 {
-	int n;
+	int i;
 
-	srand(time(0));
-	n = rand () - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n"' n);
-	} else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	} else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
+	i = 98;
+	positive_or_negative(i);
+
 	return (0);
 }
