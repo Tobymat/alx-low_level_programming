d45de296bf917fac1ff2e669a484f32264650729
Toby Matt <tobematthew@gmail.com>
2022-09-16 16:15:34 +0000

    add

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <math.h>
-#include "main.h"
 
 /**
  * maxPrimeFactors - function to check for largest prime number
@@ -9,46 +8,26 @@
  * Return: 0 if false or 1 if otherwise
  */
 
-long long maxPrimeFactors(long long n)
+int maxPrimeFactors(int n)
 {
-	/* Intialize the maximum prime factor 
-	 * variable with the lowest one
-	 */
-	long long maxPrime = -1;
+	int i, max = -1;
 
-	/* print the number 2s that can divide n */
 	while (n % 2 == 0)
 	{
-		maxPrime = 2;
-		n /= 2;
+		max = 2;
+		n = n/2;
 	}
-	/* n must be odd at this point */
-	while (n % 3 == 0)
-	{
-		maxPrime = 3;
-		n /= 3;
-	}
-	/* now we need to iterate for integers
-	 * who does not have prime factor 2 and 3
-	 */
-	for (int i = 5; i <= sqrt(n); i += 6)
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
 		{
-			maxPrime = i;
-			n /= i;
-		}
-		while (n % (i + 2) == 0)
-		{
-			maxPrime = i + 2;
-			n /= (i + 2);
+			max = i;
+			n = n/i;
 		}
 	}
-	/* This condition is handle the case when n is a prime
-	 * number greate than 4
-	 */
-	if (n > 4)
-		maxPrime = n;
-
-	return maxPrime;
+	if (n > 2)
+	{
+		max = n;
+	}
+	return max;
 }
