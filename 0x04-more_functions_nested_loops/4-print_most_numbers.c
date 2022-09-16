4cc8bf4ce3645b185e8d7113e4224e9726a2559d
Toby Matt <tobematthew@gmail.com>
2022-09-16 09:31:10 +0000

    add

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers
+ * _putchar - prints
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (c != 2 && c != 4)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
