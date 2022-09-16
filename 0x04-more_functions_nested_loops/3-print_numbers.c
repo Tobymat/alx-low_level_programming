34e89225e005babf4d7cdcc3f13ce27dcc0006ad
Toby Matt <tobematthew@gmail.com>
2022-09-16 09:21:21 +0000

    add

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * _putchar - prints
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+}
