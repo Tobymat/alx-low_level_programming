1813994a4ac4877ed21c1e538babbfa5eeae8d84
Toby Matt <tobematthew@gmail.com>
2022-09-16 10:44:15 +0000

    add

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,11 +8,18 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
+	if (n <= 0)
+		_putchar('\n');
 	for (i = 0; i < n; i++)
 	{
+		for (j = 0; j < 1; j++)
+		{
+			_putchar(' ');
+		}
 		_putchar('\\');
+		_putchar('\n');
 	}
-	_putchar('\n');
+	
 }
