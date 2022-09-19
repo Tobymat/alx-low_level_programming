a4ab5c4a8f3d4216b0e61ae26f03155abac3941d
Toby Matt <tobematthew@gmail.com>
2022-09-19 21:06:40 +0000

    add

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swap two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+	
+	 c = *a;
+	*a = *b;
+	*b = c;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the lenght of a string
+ * @s: character to check
+ *
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; i < *s; i++)
+	{
+		_putchar(i);
+	}
+}
