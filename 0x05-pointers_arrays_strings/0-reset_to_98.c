ab4721a44b2428cb510febbfb235ea27f2966ba0
Toby Matt <tobematthew@gmail.com>
2022-09-19 20:35:44 +0000

    add

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to updates the value of a pointer
+ * @n: character to update
+ *
+ * Return: Always o
+ */
+
+void reset_to_98(int *n)
+{
+	n = 98;
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN.H
+#define MAIN.H
+#include <stdlib.h>
+
+void reset_to_98(int *n);
+int _putchar(char c);
