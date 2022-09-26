f63d943bd782c4a27ab937905a87fb284c6d928c
Toby Matt <tobematthew@gmail.com>
2022-09-26 13:32:00 +0000

    add

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character
+ * @s: first character
+ * @c: second character
+ *
+ * Return: a pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	char i;
+
+	if (c[i] == 'c')
+		return (s);
+	else
+		return (NULL);
+}
