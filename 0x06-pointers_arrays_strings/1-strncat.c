e171524768401187859d40bf0334678aac28c585
Toby Matt <tobematthew@gmail.com>
2022-10-03 12:16:11 +0000

    add

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -21,8 +21,7 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[len] != '\0')
 	{
 		++len;
-	}
-	while (src[i] != '0' && i < n)
+	} while (src[i] != '0' && i < n)
 	{
 		dest[len] = src[i];
 		++i;
