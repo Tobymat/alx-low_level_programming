5cafa753f6b58fc1bc7d5323f101457929c4ec85
Toby Matt <tobematthew@gmail.com>
2022-10-03 12:41:22 +0000

    add

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,13 +14,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 	
-	for (; i < n && src[i] != '\0'; i++)
+	for (; i < n && src[i] != '\0'; ++i)
 	{
 		dest[i] = src[i];
 	}
 	while (i < n)
 	{
-		dest[i] = '\0';
+		dest[i] != '\0';
 		i++;
 	}
 	return(dest);
