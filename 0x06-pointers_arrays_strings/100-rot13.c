7a564d9ab7c224abc0af3943d26e096e4ba0e981
Toby Matt <tobematthew@gmail.com>
2022-10-04 20:04:20 +0000

    add

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,9 +10,9 @@
 char *rot13(char *s)
 {
 	int t, i;
+	
 	char n[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char r[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (t = 0; t <= 51; t++)
