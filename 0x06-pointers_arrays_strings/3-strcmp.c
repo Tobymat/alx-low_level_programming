7effe8b816b555adb0d31729385007f7917a24a0
Toby Matt <tobematthew@gmail.com>
2022-10-04 13:23:11 +0000

    add

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,12 +14,7 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0'; ++i)
 	{
-		if (s1[i] > s2[i])
-		{
-			j = s1[i] - s2[i];
-			return (j);
-		}
-		else if (s1[i] < s2[i])
+		if (s1[i] > s2[i] || s1[i] < s2[i])
 		{
 			j = s1[i] - s2[i];
 			return (j);
