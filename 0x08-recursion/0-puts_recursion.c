dc30909c148d73deb572cd20810679043184bc60
Toby Matt <tobematthew@gmail.com>
2022-10-14 15:14:24 +0000

    add

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - Entry point
+ * @s: character to check
+ * Description: a function that prints a string
+ * Return: Always 0
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+}
