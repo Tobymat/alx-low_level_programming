735130788fafebbc7f480e45a696f154d453c340
Toby Matt <tobematthew@gmail.com>
2022-10-21 05:43:45 +0000

    add

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,6 +11,11 @@
 
 int main(int argc, char *argv[])
 {
-	printf("The program name is: %s\n", argv[0]);
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("The program name is: %s\n", argv[i]);
+	}
 	return (0);
 }
