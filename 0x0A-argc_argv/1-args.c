f2e5a10596ea0df32885407c807d13adf0f0751d
Toby Matt <tobematthew@gmail.com>
2022-10-22 09:36:41 +0000

    add

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: inputted variables
+ * Description: a program that prints number of arguments
+ * Return: Number of argc
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("The number of arguments is: %d", argc);
+	return (0);
+}
