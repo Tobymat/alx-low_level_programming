7ed25ad2411f8d89cec3db2468f48dd155b1575d
Toby Matt <tobematthew@gmail.com>
2022-10-22 20:12:12 +0000

    add

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,17 +11,29 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j;
+	int count, i, j, result;
 
-	if (argc > 2)
+	count = i = j = result = 0;
+	if (argc > 0)
 	{
-		printf("input first number: ");
-		scanf("%d\n", &i);
-		printf("input second number: ");
-		scanf("%d\n", &j);
-
-		mul = i * j;
-
-		printf("The multiple of %d and %d is: %d\n", i, j, mul);
+		while (count < argc)
+		{
+			if (argc == 3)
+			{
+				if (count == 1)
+					i = atoi(argv[count]);
+				if (count == 2)
+					j = atoi(argv[count]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+			result = (i * j);
+			count++;
+		}
+		printf("%d\n", result);
 	}
+	return (0);
 }
